b9aed358d84e6c26d4f17a76a776b9f6d2bb37c9
Marertine <rhodrirhamantus@gmail.com>
2026-01-23 17:02:37 +1100

    Ch4-10: Generating text embeds

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,3 +1,4 @@
+import string
 from sentence_transformers import SentenceTransformer
 
 class SemanticSearch:
@@ -5,10 +6,27 @@ class SemanticSearch:
         # Load the model (downloads automatically the first time)
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
 
-    def msf_encode(self, text):
-        #Placeholder for me to remember how the lesson said to call the encode method when we get to that part of the coursework
-        return self.model.encode(text)
+    def generate_embedding(self, text):        
+        if len(text.strip()) == 0:
+            raise ValueError("text cannot be empty or white space")
+        
+        input_list = []  #The encode method expects a list of inputs
+
+        #For now, we only have a single term but add it to the list to allow .encode to operate
+        input_list.append(text)
+
+        return self.model.encode(input_list)[0]
+
     
+
+
+def embed_text(text):
+    ss = SemanticSearch()
+
+    embedding = ss.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
 
 
 def verify_model():
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import verify_model
+from lib.semantic_search import embed_text, verify_model
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -15,12 +15,17 @@ def main():
     # variable = subparsers.add_parser is needed when we need subsequent lines to add parameters via .add_argument
 
 
+    embed_text_parser = subparsers.add_parser("embed_text", help="Generate the embedded values from text")
+    embed_text_parser.add_argument("text", type=str, help="embed_text text")
 
 
     args = parser.parse_args()
 
     match args.command:
-        case "verify":
+        case "embed_text":
+            embed_text(args.text)
+
+        case "verify":            
             verify_model()
 
         case _:
